f468a15e7f5c43482a3b8c46d99aea4de8cffaf3
OMora17 <49887547+OMora17@users.noreply.github.com>
2022-10-07 12:17:17 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import PySimpleGUI as sg
+# This is a sample Python script.
 
-sg.theme('DarkTeal9')
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-layout=[
-    [sg.Text('Please fill out the following fields:')],
-    [sg.Text('Name',size=(15,1)),sg.InputText(key='Name')],
-    [sg.Submit(),sg.Exit()]
-]
+import xlwt
+#import xlrd
 
-window = sg.Window('Simple data entry form',layout)
-
-while True:
-    event, values = window.read()
-    if event
+# read Excel file and sheet by name
+#workbook = xlrd.open_workbook('/home/vanx/Documents/example1.xlsx')
+#sheet = workbook.sheet_by_name('Test')
+#sheet2 = workbook.sheet_by_index(2)
